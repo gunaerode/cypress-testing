d7d819ac042c5d3ee42b4b4cae1c6b79a78f0dff
Guna <37032091+gunaerode@users.noreply.github.com>
2022-09-19 09:07:32 +0530

    progressbar challenge :fire: :heart_eyes:

--- cypress/e2e/7.7.retry-ability.cy.ts
+++ cypress/e2e/7.7.retry-ability.cy.ts
@@ -1,9 +1,9 @@
-Cypress.config('defaultCommandTimeout', 16000); // wait until 16 sec overwrite defautlCommantTimeout 4 secs to 16
+// Cypress.config('defaultCommandTimeout', 16000); // wait until 16 sec overwrite defautlCommantTimeout 4 secs to 16
 describe('Retry-ability', () => {
-  it('Page load delay', () => {
+  it.skip('Page load delay', () => {
     cy.visit('/loaddelay', { timeout: 30000 }); // 30 sec waits to load page
   });
-  it('Client delay', () => {
+  it.skip('Client delay', () => {
     cy.visit('/clientdelay');
     //Press the button below and wait for data to appear (15 seconds) but cypress default only waits until 4 secs
     cy.get('#ajaxButton').click();
@@ -11,5 +11,10 @@ describe('Retry-ability', () => {
       'have.text',
       'Data calculated on the client side.'
     );
+  });
+  it.only('Loading complete message test', () => {
+    cy.visit('/progressbar');
+    cy.get('#startButton').click();
+    cy.get('#progressBar', { timeout: 30000 }).should('have.text', '100%');
   });
 });
